7e68f579b6cb9c75e30d56db4f3af3fe4f49fe1f
Pavel Šesták <pavel.sestak10@gmail.com>
2019-11-06 19:23:06 +0000

    Add new file

---
+++ 1.term BIT/IZP - Introduction to Programming Systems/pc_labs/Lab_1/hello.c
@@ -0,0 +1,32 @@
+ /************************************************/
+    /*                                              */		
+    /*              gitlab.com/sestak               */
+    /*                                              */          
+    /*              github.com/alepir               */
+    /*                                              */
+    /*          Pavel Sestak (xsesta07)             */
+    /*                                              */
+    /*              7. 10. 2019                     */
+    /*                                              */
+    /*      Introduction to Programming Systems     */
+    /*                                              */
+    /*              1st year BITP                   */
+    /*                                              */
+    /*      VUT Faculty of Information Technology   */
+    /*                                              */
+    /************************************************/  
+
+/*
+Launch example: ./hello
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//entry point
+int main ()
+{
+    printf("Hello world \n");
+    return 0;
+}
